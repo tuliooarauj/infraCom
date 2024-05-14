5f6738d75f238ae1832e1f3eb73724d7d9a7d52f
toa <toa@g2c12.cin.ufpe.br>
2024-05-14 09:28:43 -0300

    some updates in client code

--- AppRedes/cliente.py
+++ AppRedes/cliente.py
@@ -1,14 +1,16 @@
 from socket import socket, AF_INET, SOCK_STREAM
+#Passo 1: Criando o socket.
+mClientSocket = socket(AF_INET, SOCK_STREAM)
+#Passo 2: Transformando o socket criado num socket de um cliente,
+#ou seja, colacando para realizar solicitações.
+mClientSocket.connect(('localhost', 1235))
 
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect(('127.0.0.1', 12345))
-
-for i in range(3):
-    print(f'id da req: {i}')
-    data = f'Hellor, server {i}'
-
-    client_socket.send(data.encode())
-
-    rep = client_socket.recv(1024)
-
-    print(f'Reply from server {rep.decode()}')
+while True:
+    # Este loop foi criado apenas para que o cliente conseguisse enviar múltiplas solicitações
+    message = input('>>')
+    #Enviando a mensagem pelo socket criado.
+    mClientSocket.send(message.encode())
+    #Recebendo as respostas do servidor.
+    data = mClientSocket.recv(2048)
+    reply = data.decode()
+    print(f'Resposta recebida:{reply}')
